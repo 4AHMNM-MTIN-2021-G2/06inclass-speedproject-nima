b53d262a596526d1ef7064dfc519e3d6d49d4e2d
maringernina <nina.maringer01@htl-salzburg.ac.at>
2021-01-25 10:26:46 +0100

    2501 finish whiteboxing and scene manager - have to finish intro, main and end scene

---
+++ Assets/MyGame/Scripts/SpeedProject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SpeedProject : MonoBehaviour
+{
+    public void LoadMain ()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void LoadEnd()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
+    public void LoadIntro()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MyGame/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Timer.time;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Timer.time - startTime;
+
+        string minutes = ((int)time / 60).ToString();
+        string seconds = (time % 60).ToString();
+
+        timer.text = minutes + ":" + seconds;
+    }
+}
